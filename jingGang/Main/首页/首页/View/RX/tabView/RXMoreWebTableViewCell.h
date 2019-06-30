e4828b569a69474b9413c87852f1d2a2b0fc46ea
rongxu <18827279647@163.com>
2019-06-30 20:51:26 +0800

    功能界面

---
+++ jingGang/Main/首页/首页/View/RX/tabView/RXMoreWebTableViewCell.h
@@ -0,0 +1,22 @@
+//
+//  RXMoreWebTableViewCell.h
+//  jingGang
+//
+//  Created by 荣旭 on 2019/6/30.
+//  Copyright © 2019年 dengxf_dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RXMoreWebTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *namelabel;
+@property (weak, nonatomic) IBOutlet UIButton *gengButton;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageZhou;
+@property (weak, nonatomic) IBOutlet UIImageView *imageYue;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ jingGang/NetWork(网络层)/apis/request/RXUserOtherRequest.h
@@ -0,0 +1,18 @@
+//
+//  RXRXUserOtherRequest.h
+//  jingGang
+//
+//  Created by 荣旭 on 2019/6/29.
+//  Copyright © 2019年 dengxf_dev. All rights reserved.
+//
+
+#import "IRequest.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RXUserOtherRequest : AbstractRequest
+@property(nonatomic,strong)NSString*id;
+
+@end
+
+NS_ASSUME_NONNULL_END
